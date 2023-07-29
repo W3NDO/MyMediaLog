9411a358c5a89f7a6b77758bc693dc3d429ac628
w3ndo <wendonyang@gmail.com>
2023-07-29 10:16:04 +0300

    feat-incomplete(develop-pw/media_views) - created a new index partial to show the media of a specific type

--- app/reflexes/home_reflex.rb
+++ app/reflexes/home_reflex.rb
@@ -3,4 +3,14 @@ class HomeReflex < ApplicationReflex
     media = element.dataset[:media]
     morph "#test", media.gsub("_", " ")
   end
+
+  def showMedia
+    media = element.dataset[:media].to_sym
+    morph "#container", render(partial: "media/index_media", locals: {medium: media})
+  end
+
+  def showNewMediaForm
+    medium = Medium.new
+    morph "#container", render(partial: "media/form", locals: {medium: medium})
+  end
 end
