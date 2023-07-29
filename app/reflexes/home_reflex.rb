00a77056d82dfbd3f9687bc0d8a333df9a6281b6
w3ndo <wendonyang@gmail.com>
2023-07-29 16:04:37 +0300

    chore(main) - code cleanup

--- app/reflexes/home_reflex.rb
+++ app/reflexes/home_reflex.rb
@@ -4,10 +4,10 @@ class HomeReflex < ApplicationReflex
     morph "#test", media.gsub("_", " ")
   end
 
-  def showMedia
-    media = element.dataset[:media].to_sym
-    morph "#focus", render(partial: "home/new_media_button", locals: {media_type: media})
-    morph "#container", render(partial: "media/index_media", locals: {medium: media})
+  def showMedia(media_type: nil)
+    media_type = media_type || element.dataset[:media].to_sym
+    morph "#focus", render(partial: "home/new_media_button", locals: {media_type: media_type})
+    morph "#container", render(partial: "media/index_media", locals: {medium_type: media_type})
   end
 
   def showNewMediaForm
@@ -35,6 +35,18 @@ class HomeReflex < ApplicationReflex
     else
       show_notification("Failed to update rating. Please try again later")
     end
-    # morph :nothing
+  end
+
+  def deleteMedia
+    medium = Medium.find(element.dataset[:id])
+    temp = medium.dup
+    pp "CLONE => #{temp}"
+
+    if medium.destroy
+      show_notification("Successfully Deleted #{medium.media_name}")
+    else
+      show_notification("Failed to Delete #{medium.media_name}. Please try again later")
+    end
+    morph "#container", render(partial: "media/index_media", locals: {medium_type: temp.media_type})
   end
 end
